494ca0b8409ba0ec5f1ba0c7bd51f958152a3a42
zzzzzyc <104435384+zzzzzyc@users.noreply.github.com>
2022-05-25 08:49:02 +0800

    Create config.inc.php

---
+++ 试用版/爱情墙3/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_ADDRESS","localhost");
+
+define("ACCOUNT_NAME","root");
+
+define("PASSWORD","password");
+
+define("DB_NAME","TEST");
+
+define("TABLE_NAME","message");
+
+define("FIELD_NAME","message");
+>
